19a1674f90b291b77f64e3b044f53fd15a5b3c16
bernard chisumo <bchisumo74@gmail.com>
2022-09-15 11:14:10 +0100

    Views Components Created

---
+++ src/views/ContactManager.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Contact Manager</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContactManager"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/ViewContact.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Vue Contact</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ViewContact"
+}
+</script>
+
+<style scoped>
+
+</style>
