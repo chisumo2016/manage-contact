94177e9b544b5e2f1bfe80a2e9c93df1fdd77e0d
bernard chisumo <bchisumo74@gmail.com>
2022-09-15 21:18:52 +0100

    Implemented the Logic to create new contact

--- src/views/AddContact.vue
+++ src/views/AddContact.vue
@@ -8,52 +8,97 @@
     </div>
   </div>
   <!-- End of Container  -->
+
     <div class="container mt-3">
        <div class="row">
           <div class="col-md-4">
-            <form action="">
+            <form @submit.prevent="ContactCreate()">
               <div class="mb-2">
-                <input type="text" class="form-control" placeholder="Enter Your Name">
+                <input v-model="contact.name" type="text" class="form-control" placeholder="Enter Your Name">
               </div>
               <div class="mb-2">
-                <input type="text" class="form-control" placeholder="Enter Photo URL">
+                <input v-model="contact.photo" type="text" class="form-control" placeholder="Enter Photo URL">
               </div>
               <div class="mb-2">
-                <input type="email" class="form-control" placeholder="Enter Your Email">
+                <input v-model="contact.email" type="email" class="form-control" placeholder="Enter Your Email">
               </div>
               <div class="mb-2">
-                <input type="number" class="form-control" placeholder="Enter Your Mobile">
+                <input v-model="contact.mobile" type="number" class="form-control" placeholder="Enter Your Mobile">
               </div>
               <div class="mb-2">
-                <input type="text" class="form-control" placeholder="Enter Your Company">
+                <input v-model="contact.company" type="text" class="form-control" placeholder="Enter Your Company">
               </div>
               <div class="mb-2">
-                <input type="text" class="form-control" placeholder="Enter Your Title">
+                <input v-model="contact.title" type="text" class="form-control" placeholder="Enter Your Title">
               </div>
-              <div class="mb-2">
-                <select name="" id="" class="form-control">
+              <div class="mb-2" v-if="groups.length > 0">
+                <select v-model="contact.groupId" name="" id="" class="form-control">
                   <option value="">Select Group</option>
+                  <option :value="group.id" v-for="group in groups" :key="group.id">{{ group.name}}</option>
                 </select>
               </div>
               <div class="mb-2">
-                <input type="text" class="btn btn-success" value="Create">
+                <input type="submit" class="btn btn-success" value="Create">
               </div>
             </form>
           </div>
          <div class="col-md-4">
-           <img src="https://www.kindpng.com/picc/m/269-2697881_computer-icons-user-clip-art-transparent-png-icon.png" alt="" class="contact-img">
+           <img :src="contact.photo" alt="" class="contact-img">
          </div>
        </div>
     </div>
-
+<!--    <pre>{{ contact}}</pre>-->
 </template>
 
 <script>
+import {ContactService} from "@/services/ContactService";
+
 export default {
-  name: "AddContact"
+  name: "AddContact",
+
+  /** Declare the fields from database*/
+  data(){
+    return {
+      contact:{
+        name : '',
+        company : '',
+        email : '',
+        title : '',
+        mobile : '',
+        photo : '',
+        groupId : '',
+      },
+      groups:[]
+    }
+  },
+ async created() {
+    try{
+      let response = await ContactService.getAllGroups();
+      this.groups  = response.data;
+    }catch(error){
+      console.log(error);
+    }
+  },
+  methods:{
+    async ContactCreate(){
+      try{
+        /**Response from ContactService*/
+        let response  = await ContactService.createContact(this.contact); //filled in the form
+        /**Success Response*/
+        if (response){
+          /**Redirect to home page*/
+          return this.$router.push('/')
+        }else {
+          /**Stay onn the say page */
+          return this.$router.push('/contacts/add')
+        }
+      }catch (e) {
+        console.log(e);
+      }
+    }
+  }
 }
 </script>
-
 <style scoped>
 
 </style>
