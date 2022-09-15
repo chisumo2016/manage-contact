8a621aaf3f1d5ee80da1a312cd31d92f60355f9f
bernard chisumo <bchisumo74@gmail.com>
2022-09-15 23:04:24 +0100

    Implemented the logic to update the contact record

--- src/views/EditContact.vue
+++ src/views/EditContact.vue
@@ -2,6 +2,7 @@
   <div class="container mt-3">
     <div class="row">
       <div class="col">
+<!--        <pre>{{ contact}}</pre>-->
         <p class="h3 text-success fw-bold">Edit Contact</p>
         <p class="fst-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur deserunt ea eligendi et, eum excepturi, facere in libero mollitia nisi omnis quae qui quidem quos reiciendis rem repellendus unde velit.</p>
       </div>
@@ -10,37 +11,38 @@
     <div class="container mt-3">
       <div class="row">
         <div class="col-md-4">
-          <form action="">
+          <form @submit.prevent="updateContact()">
             <div class="mb-2">
-              <input type="text" class="form-control" placeholder="Enter Your Name">
+              <input  v-model="contact.name" type="text" class="form-control" placeholder="Enter Your Name">
             </div>
             <div class="mb-2">
-              <input type="text" class="form-control" placeholder="Enter Photo URL">
+              <input  v-model="contact.photo" type="text" class="form-control" placeholder="Enter Photo URL">
             </div>
             <div class="mb-2">
-              <input type="email" class="form-control" placeholder="Enter Your Email">
+              <input v-model="contact.email" type="email" class="form-control" placeholder="Enter Your Email">
             </div>
             <div class="mb-2">
-              <input type="number" class="form-control" placeholder="Enter Your Mobile">
+              <input  v-model="contact.mobile" type="number" class="form-control" placeholder="Enter Your Mobile">
             </div>
             <div class="mb-2">
-              <input type="text" class="form-control" placeholder="Enter Your Company">
+              <input v-model="contact.company" type="text" class="form-control" placeholder="Enter Your Company">
             </div>
             <div class="mb-2">
-              <input type="text" class="form-control" placeholder="Enter Your Title">
+              <input v-model="contact.title" type="text" class="form-control" placeholder="Enter Your Title">
             </div>
             <div class="mb-2">
-              <select name="" id="" class="form-control">
+              <select  v-model="contact.groupId" v-if="groups.length > 0" name="" id="" class="form-control">
                 <option value="">Select Group</option>
+                <option :value="group.id" v-for="group of groups" :key="group.id">{{group.name}}</option>
               </select>
             </div>
             <div class="mb-2">
-              <input type="text" class="btn btn-success" value="Record Update">
+              <input type="submit" class="btn btn-success" value="Record Update">
             </div>
           </form>
         </div>
         <div class="col-md-4">
-          <img src="https://www.kindpng.com/picc/m/269-2697881_computer-icons-user-clip-art-transparent-png-icon.png" alt="" class="contact-img">
+          <img :src="contact.photo" alt="" class="contact-img">
         </div>
       </div>
     </div>
@@ -48,8 +50,59 @@
 </template>
 
 <script>
+import {ContactService} from "@/services/ContactService";
+
 export default {
-  name: "EditContact"
+  name: "EditContact",
+  data(){
+    return{
+      contactId : this.$route.params.contactId,
+      loading : false,
+      contact:{  //object
+        name : '',
+        company : '',
+        email : '',
+        title : '',
+        mobile : '',
+        photo : '',
+        groupId : '',
+      },
+      errorMessage: null,
+      groups : []
+    }
+  },
+  async created() {
+    try {
+      this.loading  = true;
+      let response = await  ContactService.getContact(this.contactId);
+      let groupsResponse  = await  ContactService.getAllGroups();
+      this.contact = response.data;
+      this.groups  = groupsResponse.data
+      this.loading  = false;
+    }catch (e) {
+      this.errorMessage = e;
+      this.loading = false;
+
+    }
+  },
+  methods:{
+    async updateContact(){
+      try{
+        /**Response from ContactService*/
+        let response  = await ContactService.updateContact(this.contact, this.contactId); //filled in the form
+        /**Success Response*/
+        if (response){
+          /**Redirect to home page*/
+          return this.$router.push('/')
+        }else {
+          /**Stay onn the say page */
+          return this.$router.push(`/contacts/edit/${this.contactId}`);
+        }
+      }catch (e) {
+        console.log(e);
+      }
+    }
+  }
 }
 </script>
 
