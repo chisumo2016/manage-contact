b06bd9b0df368ad8ad4fd834abedd5ebec21ab51
bernard chisumo <bchisumo74@gmail.com>
2022-09-15 13:51:50 +0100

    Static Page for EditContact UI

--- src/views/EditContact.vue
+++ src/views/EditContact.vue
@@ -1,5 +1,50 @@
 <template>
-  <h2>Edit Contact</h2>
+  <div class="container mt-3">
+    <div class="row">
+      <div class="col">
+        <p class="h3 text-success fw-bold">Edit Contact</p>
+        <p class="fst-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur deserunt ea eligendi et, eum excepturi, facere in libero mollitia nisi omnis quae qui quidem quos reiciendis rem repellendus unde velit.</p>
+      </div>
+    </div>
+    <!-- End of Container  -->
+    <div class="container mt-3">
+      <div class="row">
+        <div class="col-md-4">
+          <form action="">
+            <div class="mb-2">
+              <input type="text" class="form-control" placeholder="Enter Your Name">
+            </div>
+            <div class="mb-2">
+              <input type="text" class="form-control" placeholder="Enter Photo URL">
+            </div>
+            <div class="mb-2">
+              <input type="email" class="form-control" placeholder="Enter Your Email">
+            </div>
+            <div class="mb-2">
+              <input type="number" class="form-control" placeholder="Enter Your Mobile">
+            </div>
+            <div class="mb-2">
+              <input type="text" class="form-control" placeholder="Enter Your Company">
+            </div>
+            <div class="mb-2">
+              <input type="text" class="form-control" placeholder="Enter Your Title">
+            </div>
+            <div class="mb-2">
+              <select name="" id="" class="form-control">
+                <option value="">Select Group</option>
+              </select>
+            </div>
+            <div class="mb-2">
+              <input type="text" class="btn btn-success" value="Record Update">
+            </div>
+          </form>
+        </div>
+        <div class="col-md-4">
+          <img src="https://www.kindpng.com/picc/m/269-2697881_computer-icons-user-clip-art-transparent-png-icon.png" alt="" class="contact-img">
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
