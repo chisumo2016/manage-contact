62462388e773afad2424e2fdf2083b035f808290
bernard chisumo <bchisumo74@gmail.com>
2022-09-15 22:02:42 +0100

    Implemented the logic to show the contact

--- src/views/ShowContact.vue
+++ src/views/ShowContact.vue
@@ -3,25 +3,48 @@
     <div class="container mt-3">
       <div class="row">
         <div class="col">
-          <p class="h3 text-success fw-bold">View Contact</p>
+
+          <p class="h3 text-success fw-bold">View Contact </p>
           <p class="fst-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur deserunt ea eligendi et, eum excepturi, facere in libero mollitia nisi omnis quae qui quidem quos reiciendis rem repellendus unde velit.</p>
         </div>
       </div>
     </div>
   </div><!--End of Container-->
-  <div class="container">
+  <!--  Display Spinner-->
+  <div v-if="loading">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <Spinner/>
+        </div>
+      </div>
+    </div>
+  </div><!--  End of Spinner-->
+
+  <!--  Error Message-->
+  <div v-if="!loading && errorMessage">
+    <div class="container mt-3">
+      <div class="row">
+        <div class="col">
+          <p class="h4 text-danger fw-bold">{{ errorMessage}}</p>
+        </div>
+      </div>
+    </div>
+  </div> <!-- End of  Error Message-->
+
+  <div class="container" v-if="!loading && isDone()">
       <div class="row align-items-center">
         <div class="col-md-4">
-          <img src="https://www.kindpng.com/picc/m/269-2697881_computer-icons-user-clip-art-transparent-png-icon.png" alt="" class="contact-img-show">
+          <img :src="contact.photo" alt="" class="contact-img-show">
         </div>
         <div class="col-md-6">
           <ul class="list-group">
-            <li class="list-group-item">Name: <span class="fw-bold"> Name</span></li>
-            <li class="list-group-item">Email: <span class="fw-bold"> Email</span></li>
-            <li class="list-group-item">Mobile: <span class="fw-bold"> Mobile</span></li>
-            <li class="list-group-item">Company: <span class="fw-bold"> Name</span></li>
-            <li class="list-group-item">Title: <span class="fw-bold"> Email</span></li>
-            <li class="list-group-item">Group: <span class="fw-bold"> Mobile</span></li>
+            <li class="list-group-item">Name: <span class="fw-bold">{{ contact.name }}</span></li>
+            <li class="list-group-item">Email: <span class="fw-bold">{{ contact.email }}</span></li>
+            <li class="list-group-item">Mobile: <span class="fw-bold">{{ contact.mobile }}</span></li>
+            <li class="list-group-item">Company: <span class="fw-bold">{{ contact.company}}</span></li>
+            <li class="list-group-item">Title: <span class="fw-bold">{{ contact.title }}</span></li>
+            <li class="list-group-item">Group: <span class="fw-bold">{{ group.name }}</span></li>
           </ul>
         </div>
       </div>
@@ -34,8 +57,41 @@
 </template>
 
 <script>
+import {ContactService} from "@/services/ContactService";
+import Spinner from "@/components/Spinner";
+
 export default {
-  name: "ViewContact"
+  name: "ShowContact",
+  components: {Spinner},
+  data(){
+    return{
+      contactId : this.$route.params.contactId,
+      loading :false,
+      contact: {},
+      errorMessage : null,
+      group: {},
+    }
+  },
+  async created() {
+    /**Contact Particular Data*/
+    try {
+      this.loading = true;
+      let response = await  ContactService.getContact(this.contactId);
+      let groupResponse = await  ContactService.getGroup(response.data);
+      this.contact = response.data;
+      this.group = groupResponse.data;
+      this.loading = false;
+    }catch (e) {
+      this.errorMessage = e;
+      this.loading = false;
+    }
+  },
+  methods:{
+    /**Checking if the objects are not empty*/
+    isDone(){
+      return Object.keys(this.contact).length >0 && Object.keys(this.group).length > 0;
+    }
+  }
 }
 </script>
 
