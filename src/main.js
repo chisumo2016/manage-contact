544dc01bf4382da3f3b29fe5fdf12af47da340e4
bernard chisumo <bchisumo74@gmail.com>
2022-09-15 11:10:20 +0100

    Packages Installation and Configuration(Bootstrap,JS ,Fotawesom)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+/**FontAwesome Icon*/
+import "../node_modules/@fortawesome/fontawesome-free/css/all.css"
+
+/**Bootstrap CSS , JS Configuration*/
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+
+/** Main Styles CSS*/
+import "./styles.css"
 
 createApp(App).use(store).use(router).mount('#app')
