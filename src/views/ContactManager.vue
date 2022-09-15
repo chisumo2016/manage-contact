12960e4e470e9c0810195ca46cd3f075a0bffddd
bernard chisumo <bchisumo74@gmail.com>
2022-09-15 16:53:07 +0100

    Fetching Data from the server and display on Conntact Manager UI

--- src/views/ContactManager.vue
+++ src/views/ContactManager.vue
@@ -26,20 +26,41 @@
        </div>
      </div>
    </div>  <!-- End of Container -->
-  <div class="container mt-3">
+<!--  Display Spinner-->
+  <div v-if="loading">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <Spinner/>
+        </div>
+      </div>
+    </div>
+  </div><!--  End of Spinner-->
+
+ <!--  Error Message-->
+  <div v-if="!loading && errorMessage">
+    <div class="container mt-3">
+      <div class="row">
+        <div class="col">
+          <p class="h4 text-danger fw-bold">{{ errorMessage}}</p>
+        </div>
+      </div>
+    </div>
+  </div> <!-- End of  Error Message-->
+  <div class="container mt-3" v-if="contacts.length > 0">
     <div class="row">
-      <div class="col-md-6">
+      <div class="col-md-6"  v-for="contact in contacts" :key="contact">
         <div class="card my-2 list-group-item-success shadow-lg">
           <div class="card-body">
             <div class="row align-items-center">
               <div class="col-sm-4">
-                <img src="https://www.kindpng.com/picc/m/269-2697881_computer-icons-user-clip-art-transparent-png-icon.png" alt="" class="contact-img">
+                <img :src="contact.photo" alt="" class="contact-img">
               </div>
               <div class="col-sm-7">
                   <ul class="list-group">
-                    <li class="list-group-item">Name: <span class="fw-bold"> Name</span></li>
-                    <li class="list-group-item">Email: <span class="fw-bold"> Email</span></li>
-                    <li class="list-group-item">Mobile: <span class="fw-bold"> Mobile</span></li>
+                    <li class="list-group-item">Name: <span class="fw-bold">{{contact.name}}</span></li>
+                    <li class="list-group-item">Email: <span class="fw-bold"> {{contact.email}}</span></li>
+                    <li class="list-group-item">Mobile: <span class="fw-bold"> {{contact.mobile}}</span></li>
                   </ul>
               </div>
               <div class="col-sm-1 d-flex flex-column justify-content-center align-items-center">
@@ -57,11 +78,48 @@
 </template>
 
 <script>
+import {ContactService} from "@/services/ContactService";
+import Spinner from "@/components/Spinner";
+
 export default {
-  name: "ContactManager"
+  name: "ContactManager",
+  components: {Spinner},
+  /** Declare the properties/Data*/
+  data(){
+    return {
+      loading: false,
+      contacts: [],
+      errorMessage: null
+    }
+  },
+
+  /** Life Cycle Hooks*/
+  async created() {
+    try{
+        this.loading = true;
+        let response = await  ContactService.getAllContacts();
+        //let response = await  this.getAllContactsData();
+        this.contacts = response.data;
+        this.loading = false;
+    }catch (error){
+      this.errorMessage = error;
+      this.loading = false
+    }
+  },
+  methods : {
+
+
+  }
 }
 </script>
 
 <style scoped>
 
 </style>
+
+
+<!--//getAllContactsData:function(){},-->
+<!--getAllContactsData : async() => {-->
+
+<!--//return ContactService.getAllContacts()-->
+<!--},-->
